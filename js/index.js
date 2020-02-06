a4ee9c85494a0a2b382a5f0aae063dfcc0b08547
EnzoRobaina <enzo.robaina@gmail.com>
2020-02-06 19:03:33 -0300

    Adiciona evento ao trocar a dimensão

--- js/index.js
+++ js/index.js
@@ -18,3 +18,33 @@ console.log(multiplicarPorEscalar(v1, 2))
 console.log(calcularProjecao(v1, v2))
 
 console.log(ortonormalizarGramSchmidt([v1, v2, v3]))
+
+$(document).ready(function(){
+    function getTamanhoVetor(dimensao){
+        return `<div class="col-md-${(dimensao == 5) ? 2 : Math.floor((12-3)/dimensao)}"><input type="number" class="form-control"></div>`.repeat(dimensao)
+        + `<div class="col-md-${(dimensao == 5) ? 1: 2}"><button class = "btn btn-block btn-primary">I</button></div>`
+    }
+
+    function removerInputs(){
+        $("form").find("input[type='number']").parents(".form-group.row.row-vetor").remove()
+    }
+
+    $("#dimensao").on('change', function(){
+        removerInputs()
+        let dimensao = $(this).val()
+
+        for (let i = 0; i < dimensao; i++){
+            $("form").append(
+                `<div class="form-group row row-vetor">
+                    <label class="col-md-1 col-form-label">#${i+1}:</label>
+                    ${getTamanhoVetor(dimensao)}
+                </div>`
+            )
+        }
+    })
+
+    
+
+    
+})
+
