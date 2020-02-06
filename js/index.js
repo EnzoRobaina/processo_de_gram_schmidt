5e82e53299d44a074ee1cb8a6e50cf7303c4cacf
EnzoRobaina <enzo.robaina@gmail.com>
2020-02-06 00:56:57 -0300

    Importa funções p/ index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
-import { produtoEscalar } from './mat.js'
+import { 
+    calcularProdutoEscalar, 
+    calcularNorma, 
+    subtrairVetores, 
+    multiplicarPorEscalar, 
+    calcularProjecao,
+    ortonormalizarGramSchmidt
+} from './mat.js'
 
-console.log(produtoEscalar(
-    [1,2,3],
-    [4,5,6]
-))
+const v1 = [1, 2, 3]
+const v2 = [4, 5, 6]
+const v3 = [7, 8, 9]
+
+console.log(subtrairVetores(v2, v1))
+
+console.log(multiplicarPorEscalar(v1, 2))
+
+console.log(calcularProjecao(v1, v2))
+
+console.log(ortonormalizarGramSchmidt([v1, v2, v3]))
