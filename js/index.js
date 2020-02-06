bc24770c5603da8afb26cf828ba206ede51501ec
EnzoRobaina <enzo.robaina@gmail.com>
2020-02-06 20:25:06 -0300

    Finaliza funcionalidade básica

--- js/index.js
+++ js/index.js
@@ -5,29 +5,23 @@ import {
     multiplicarPorEscalar, 
     calcularProjecao,
     ortonormalizarGramSchmidt,
-    obterNumeroAleatorio
+    obterNumeroAleatorio,
+    toFixedVetor
 } from './mat.js'
 
 const v1 = [1, 2, 3]
 const v2 = [4, 5, 6]
 const v3 = [7, 8, 9]
 
-console.log(subtrairVetores(v2, v1))
-
-console.log(multiplicarPorEscalar(v1, 2))
-
-console.log(calcularProjecao(v1, v2))
-
-console.log(ortonormalizarGramSchmidt([v1, v2, v3]))
-
 $(document).ready(function(){
     function getTamanhoVetor(dimensao){
-        return `<div class="col-md-${(dimensao == 5) ? 2 : Math.floor((12-3)/dimensao)}"><input type="number" class="form-control"></div>`.repeat(dimensao)
+        return `<div class="col-md-${(dimensao == 5) ? 2 : Math.floor((12-3)/dimensao)}"><input type="number" class="form-control" value="0"></div>`.repeat(dimensao)
         + `<div class="ml-auto pl-1 col-md-${(dimensao == 5) ? 1: 2}"><button class = "btn-dice btn btn-block btn-outline-primary"><i class="fas fa-dice-five"></i></button></div>`
     }
 
     function removerInputs(){
         $("form").find("input[type='number']").parents(".form-group.row.row-vetor").remove()
+        $("#btn-calcular").parents(".form-group.row").remove()
     }
 
     $("form").on('click', '.btn-dice', function(event) {
@@ -40,8 +34,9 @@ $(document).ready(function(){
 
     $("#dimensao").on('change', function(){
         removerInputs()
-        let dimensao = $(this).val()
+        if ($(this).val() == 0){ return }
 
+        let dimensao = $(this).val()
         for (let i = 0; i < dimensao; i++){
             $("form").append(
                 `<div class="form-group row row-vetor">
@@ -50,6 +45,42 @@ $(document).ready(function(){
                 </div>`
             )
         }
+        $("form").append(
+            `<div class="form-group row">
+                <div class = "ml-auto pl-1 col-md-2">
+                    <button id = "btn-calcular" class = "btn btn-block btn-success"><i class="fas fa-calculator mr-1"></i>Calcular!</button>
+                </div>
+            </div>`
+        )
+    })
+
+    function preencherModal(vetores){
+        let modal = $("#resultadoModal")
+        modal.find(".modal-body").html('')
+        let dimensao = vetores[0].length
+
+        vetores.forEach(function(vetor, indice){
+            modal.find(".modal-body").append(
+                `<div class = "py-2"><h6 class = "inline">v${indice + 1}:</h6> <h4 class = "inline">(${toFixedVetor(vetor).join(', ')})</h4></div>`
+            )
+        })
+        
+        
+        modal.modal('show')
+    }
+
+    $("form").on('click', '#btn-calcular', function(event){
+        event.preventDefault()
+        let vetores = []
+        $(".row-vetor").each(function(i, e){
+            let vetorInterno = []
+            $(this).find("input[type='number']").each(function(indice, elemento){
+                vetorInterno.push(parseInt(elemento.value))
+            })
+            vetores.push(vetorInterno)
+        })
+        console.log(ortonormalizarGramSchmidt(vetores))
+        preencherModal(ortonormalizarGramSchmidt(vetores))
     })
 })
 
--- js/mat.js
+++ js/mat.js
@@ -39,3 +39,7 @@ export function ortonormalizarGramSchmidt(listaDeVetores){
 export function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min
 }
+
+export function toFixedVetor(vetor){
+    return vetor.map(e => parseFloat(e.toFixed(2)))
+}
